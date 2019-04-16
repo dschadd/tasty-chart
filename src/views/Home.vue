d2eac3a158ae74203ef8af9bbc7e01513f29e9a8
dschadd <dschadd@gmail.com>
2019-04-15 19:19:26 -0500

    Update comments

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input v-model="search" type="text" name="Search" placeholder="Search" />
+    <button v-on:click="searchCompanies">Submit</button> {{ list }}
   </div>
 </template>
 
+<style></style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      companyNames: ["Apple", "Google", "Microsoft"],
+      header: [],
+      list: [],
+      search: ""
+    };
+  },
+  created: function() {
+    // TODO: remove empty last element of header array
+    // import the CSV of listed stocks
+    axios.get("./data.csv").then(res => {
+      // parse CSV to store separate companies as elements of an array
+      const rowData = res.data.split("\n");
+      // parse the first element of the rowData array to remove quotes ("") from the string
+      // create new header array from first row of CSV data
+      const header = rowData[0].replace(/\"/g, "").split(",");
+      // create array of objects that have a key of headers linked to specific company information
+      const list = rowData.map(row => {
+        // parse the rowData element to remove quotes ("") from the string
+        // create new array which is an array containing arrays of each company's data
+        const rowArray = row.replace(/\"/g, "").split(",");
+        // initialize object for storing this item's data
+        let obj = {};
+        // set each header's key value pair to corresponding company data
+        header.forEach((key, i) => {
+          obj[key] = rowArray[i];
+        });
+        return obj;
+      });
+      // sets data list to array of company objects
+      this.list = list;
+      console.log(list);
+    });
+  },
+  methods: {
+    searchCompanies: function() {
+      const companyNames = this.companyNames;
+      console.log(companyNames);
+    }
+  },
+  computed: {}
+};
 </script>
