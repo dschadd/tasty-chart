380826c2d7613ea51611a8928a97661d115c410a
dschadd <dschadd@gmail.com>
2019-04-16 19:13:55 -0500

    display todays price

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,11 @@
+<!--
+  TODO
+  Fix variable names
+  Dynamic typeahead search
+  Chart candlestick price
+  Ask Peter, Dan, and Jay what I can do to make the code better
+  Testing?z
+-->
 <template>
   <div class="home">
     <!-- <label for="input">Companies:</label> -->
@@ -12,6 +20,7 @@
 
     <h1>{{ search.Name }}</h1>
     <h2>{{ search.Symbol }}</h2>
+    <h2>${{ price }}</h2>
   </div>
 </template>
 
@@ -26,7 +35,8 @@ export default {
       companyNames: ["Apple", "Google", "Microsoft"],
       header: [],
       companies: [],
-      search: ""
+      search: "",
+      price: ""
     };
   },
   created: function() {
@@ -37,12 +47,12 @@ export default {
       const rowData = res.data.split("\n");
       // parse the first element of the rowData array to remove quotes ("") from the string
       // create new header array from first row of CSV data
-      const header = rowData[0].replace(/\"/g, "").split(",");
+      const header = rowData[0].replace(/"/g, "").split(",");
       // create array of objects that have a key of headers linked to specific company information
       const companies = rowData.map(row => {
         // parse the rowData element to remove quotes ("") from the string
         // create new array which is an array containing arrays of each company's data
-        const rowArray = row.replace(/\"/g, "").split(",");
+        const rowArray = row.replace(/"/g, "").split(",");
         // initialize object for storing this item's data
         let obj = {};
         // set each header's key value pair to corresponding company data
@@ -53,14 +63,14 @@ export default {
       });
       // sets data list to array of company objects
       this.companies = companies;
-      console.log(companies);
     });
   },
   methods: {
     searchCompanies: function() {
       const search = this.search;
-      var priceData = axios.get("https://api.iextrading.com/1.0/stock/aapl/chart/1m");
-      console.log(priceData);
+      axios.get(`https://api.iextrading.com/1.0/stock/${search.Symbol}/chart/1m`).then(res => {
+        this.price = res.data[0]["close"];
+      });
     }
   },
   computed: {}
